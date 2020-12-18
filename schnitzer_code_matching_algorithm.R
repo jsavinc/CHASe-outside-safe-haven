8791da0f470413ca0803f047e53d30234aa6aa03
jsavn <jsavinc@napier.ac.uk>
2020-12-18 17:29:29 +0000

    Made progress on the algorithm & started Rmd to document how it works

--- schnitzer_code_matching_algorithm.R
+++ schnitzer_code_matching_algorithm.R
@@ -1,10 +1,19 @@
-admissions_in_long_format
-
 library(dplyr)  # enables tidy processing functions
 library(fuzzyjoin)  # for regex-based joining of tables
 
+# Load data
+admissions <- read_csv("../Collaborations/Sample Set of Simulated 3 Yrs of Encounters.csv") %>% 
+  group_by(id) %>% 
+  mutate(episode_id = 1:n()) %>%  # add episode id
+  ungroup
+
 # We convert the admissions into a format where each admission has a unique identifier, and each row represents one diagnosis code
-admissions_in_long_format <- NULL  # whatever processing needs doing to get to that stage
+# admissions_in_long_format <- NULL  # whatever processing needs doing to get to that stage
+admissions_in_long_format <- 
+  admissions %>% 
+  pivot_longer(
+    cols = starts_with("diagnosis"), names_to = "source", values_to = "code"
+  )
 
 # The algorithm outline is as follows:
 # 1a Create a subset of all episodes that match any of the inclusions
@@ -12,8 +21,6 @@ admissions_in_long_format <- NULL  # whatever processing needs doing to get to t
 # 2a Find the overlap between episodes with inclusions and exclusions that match one another (using episode id for matching episodes and inclusion id for finding matching inclusions & exclusions)
 # 2b Remove the above overlap from the subset of episodes matching inclusions (i.e. keep only subset of inclusions with no matching exclusions)
 # 3 Remove inclusions that don't meet the additional requirements
-
-
 
 admissions_with_inclusions <-
   admissions_in_long_format %>%
